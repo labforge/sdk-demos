c2ba19c553fdca11649bd0e47cae19f8dde14ed3
Thomas Reidemeister <thomas@labforge.ca>
2023-05-05 21:19:01 -0400

    Rudimentary device selection

--- utility/utility.py
+++ utility/utility.py
@@ -18,14 +18,60 @@
 """
 __author__ = "Thomas Reidemeister <thomas@labforge.ca>"
 __copyright__ = "Copyright 2023, Labforge Inc."
+import eBUS as eb
 import sys
 
 
-from PySide6.QtWidgets import QApplication, QMainWindow, QStyle, QFileDialog
-from PySide6.QtGui import QIcon
-from PySide6.QtCore import QDir, QFileInfo
+from PySide6.QtWidgets import QApplication, QMainWindow, QStyle, QFileDialog, QDialog, QFormLayout, QListView, \
+    QDialogButtonBox, QMessageBox
+from PySide6.QtGui import QIcon, QStandardItemModel, QStandardItem
+from PySide6.QtCore import QDir, QFileInfo, Qt
 from PySide6.QtNetwork import QNetworkAccessManager
 from widgets import Ui_MainWindow
+
+LABFORGE_MAC_RANGE = '8c:1f:64:d0:e'
+
+
+class BottlenoseSelector(QDialog):
+    def __init__(self,  title, parent=None):
+        super(BottlenoseSelector, self).__init__(parent=parent)
+        form = QFormLayout(self)
+        self.listView = QListView(self)
+        form.addRow(self.listView)
+
+        self.setWindowTitle(title)
+
+        # Enumerate all bottlenose candidates
+        # FIXME: move this into a background thread with polling
+        model = QStandardItemModel(self.listView)
+        self.find_bottlenose(model)
+        self.listView.setModel(model)
+
+        button_box = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel, Qt.Horizontal, self)
+        form.addRow(button_box)
+        button_box.accepted.connect(self.accept)
+        button_box.rejected.connect(self.reject)
+
+    def find_bottlenose(self, model):
+        system = eb.PvSystem()
+        system.Find()
+
+        # Detect, select Bottlenose.
+        for i in range(system.GetInterfaceCount()):
+            interface = system.GetInterface(i)
+            print(f"   {interface.GetDisplayID()}")
+            for j in range(interface.GetDeviceCount()):
+                device_info = interface.GetDeviceInfo(j)
+                if device_info.GetMACAddress().GetUnicode().find(LABFORGE_MAC_RANGE) == 0:
+                    standard_item = QStandardItem(f"{device_info.GetDisplayID()}")
+                    standard_item.setSelectable(True)
+                    standard_item.setData(device_info.GetConnectionID())
+                    model.appendRow(standard_item)
+
+    def selected_bottlenose(self):
+        if len(self.listView.selectedIndexes()) > 0:
+            return self.listView.selectedIndexes()[0].data(Qt.UserRole + 1)
+        return None
 
 
 class MainWindow(QMainWindow):
@@ -62,9 +108,23 @@ class MainWindow(QMainWindow):
     def __del__(self):
         self.disconnect()
 
+    def connect_gev(self, connection_id):
+        result, device = eb.PvDevice.CreateAndConnect(connection_id)
+        if device is None:
+            QMessageBox.critical(self,
+                                 "Cannot connect",
+                                 f"Unable to connect to device: {result.GetCodeString()} ({result.GetDescription()})")
+            self.disconnect()
+        else:
+            self.device = device
+            self.ui.txtIP.setText(device.GetIPAddress().GetUnicode())
+            self.ui.txtMAC.setText(device.GetMACAddress().GetUnicode())
+        return device
+
+
     def disconnect(self):
         if self.device is not None:
-            # Fixme Free device
+            self.device = None
             pass
         # Reset network device
         self.ui.txtIP.setText("")
@@ -89,10 +149,16 @@ class MainWindow(QMainWindow):
             self.ui.txtFile.setText(selected_file)
 
     def handle_connect(self):
-        print("HandleConnect")
+        select = BottlenoseSelector("Select Bottlenose Camera", self)
+        if select.exec() == QDialog.Accepted:
+            connection_id = select.selected_bottlenose()
+            if connection_id is not None:
+                self.disconnect()
+                self.connect_gev(connection_id)
+                print(connection_id)
 
     def handle_quit(self):
-        print("HandleQuit")
+        self.close()
 
     def handle_upload_finished(self):
         print("Upload Finished")
